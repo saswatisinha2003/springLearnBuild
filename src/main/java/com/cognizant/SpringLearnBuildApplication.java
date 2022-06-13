09a0883cad7d19fe54371c8717e7c2ba880b38f8
Saswati Sinha <saswatisinha21buisness@gmail.com>
2022-06-13 13:22:56 +0530

    added the project

---
+++ src/main/java/com/cognizant/SpringLearnBuildApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnBuildApplication.class, args);
+		System.out.println("Hello spring learn build project");
+	}
+
+}
